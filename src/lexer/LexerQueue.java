d595cb557ccac0f5d654bef673a7727ca9f5e584
1Dips <ido.geva.b@gmail.com>
2024-04-19 16:05:53 +0300

    change/migrate-arraylist Switch arraylist for linked list

--- src/lexer/LexerQueue.java
+++ src/lexer/LexerQueue.java
@@ -6,31 +6,29 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class LexerQueue {
-    ArrayList<Token> tokens;
-    int current;
+    LinkedList<Token> tokens;
 
     public LexerQueue() {
-        current = 0;
-        this.tokens = new ArrayList<>();
+        this.tokens = new LinkedList<>();
     }
 
     public Token advance() {
-        return tokens.get(current++);
+        return tokens.pollFirst();
     }
 
     public Token peek() {
-        return tokens.get(current + 1);
+        return tokens.get(1);
     }
 
     public Token get() {
-        return tokens.get(current);
+        return tokens.peek();
     }
 
     public void add(Token tkn) {
         tokens.add(tkn);
     }
 
-    public ArrayList<Token> getTokens() {
+    public LinkedList<Token> getTokens() {
         return tokens;
     }
 }
